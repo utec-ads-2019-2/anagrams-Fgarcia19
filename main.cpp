35396b2bb288c3b35cd522c07a876b9b153930ca
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2019-08-25 20:30:27 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <map>
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
+string input,string_a_comparar;
+vector<string>lista_imprimir;
+map<string,pair<string,int>> lista;
+std::vector<string>::iterator it;
+
+void no_anagramas(string input){
+    string_a_comparar = input;
+    transform(string_a_comparar.begin(),string_a_comparar.end(),string_a_comparar.begin(), ::tolower);
+    sort(string_a_comparar.begin(),string_a_comparar.end());
+    lista[string_a_comparar].second++;
+    lista[string_a_comparar].first = input;
+
+
+
+}
+bool lexico(string a, string b){
+    return a<b;
 }
 
+
+
+
+int main() {
+
+    while(cin>>input){
+        if(input=="#")break;
+        no_anagramas(input);
+
+
+    }
+
+    for(auto item : lista){
+        if(item.second.second==1){lista_imprimir.push_back(item.second.first);}
+    }
+
+    sort(lista_imprimir.begin(),lista_imprimir.end(),lexico);
+
+    for(auto item : lista_imprimir){
+        cout<<item<<endl;
+    }
+
+
+    return 0;
+}
